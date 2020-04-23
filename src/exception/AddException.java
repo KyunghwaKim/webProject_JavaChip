4b03dbb6fae33db386831c56e7dc8490180910bd
kosta <kosta@192.168.0.116>
2020-04-23 11:31:34 +0900

    exception 추가

---
+++ src/exception/AddException.java
@@ -0,0 +1,13 @@
+package exception;
+
+public class AddException extends Exception {
+   public AddException() {
+      
+   }
+   
+   public AddException(String message) {
+      super(message);
+   }
+   
+
+}
---
+++ src/exception/ModifyException.java
@@ -0,0 +1,13 @@
+package exception;
+
+public class ModifyException extends Exception {
+   public ModifyException() {
+            
+   }
+
+   public ModifyException(String message) {
+      super(message);
+   }
+      
+
+}

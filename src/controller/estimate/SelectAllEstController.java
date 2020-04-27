0cf636882b55ba87044f30410bfdbab1f6d47afc
Park <sue199312@gmail.com>
2020-04-27 17:34:54 +0900

    path, 강의평 수정

--- src/controller/estimate/SelectAllEstController.java
+++ src/controller/estimate/SelectAllEstController.java
@@ -14,12 +14,15 @@ public class SelectAllEstController implements Controller {
 
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("1");
 		List<EstimateBoard> estimateList = EstimateService.selectAll();
 
 		request.setAttribute("estimateList", estimateList);
 
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("");
+		
+		
+		mv.setViewName("community/evaluation.jsp");
 
 		return mv;
 	}

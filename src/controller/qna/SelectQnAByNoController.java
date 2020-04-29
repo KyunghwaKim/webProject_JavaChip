a9bd19fc5b7c934314d8da29b10d65540d785d47
Park <sue199312@gmail.com>
2020-04-29 10:19:36 +0900

    qna 수정하기 - 수빈

--- src/controller/qna/SelectQnAByNoController.java
+++ src/controller/qna/SelectQnAByNoController.java
@@ -5,16 +5,18 @@ import javax.servlet.http.HttpServletResponse;
 
 import controller.Controller;
 import controller.ModelAndView;
+import model.domain.QnABoard;
 import model.service.QnAService;
 
 public class SelectQnAByNoController implements Controller{
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		String no = request.getParameter("no");//게시판번호
-		QnAService.selectByNo(Integer.parseInt(no));
+		QnABoard qnaBoard = QnAService.selectByNo(Integer.parseInt(no));
+		request.setAttribute("qnaBoard", qnaBoard);
 		
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("");
+		mv.setViewName("community//qnaDetail.jsp");
 		return mv;
 	}
 }

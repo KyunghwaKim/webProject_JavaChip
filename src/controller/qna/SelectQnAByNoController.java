99e3db7606497e02c31be58d531dfa700af0a341
Park <sue199312@gmail.com>
2020-05-05 13:26:02 +0900

    qna답글은 답글작성버튼 안보이게 수정  -sb

--- src/controller/qna/SelectQnAByNoController.java
+++ src/controller/qna/SelectQnAByNoController.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import controller.Controller;
 import controller.ModelAndView;
+import model.dao.impl.PersonDAOImpl;
+import model.domain.Person;
 import model.domain.QnABoard;
 import model.service.QnAService;
 
@@ -13,6 +15,12 @@ public class SelectQnAByNoController implements Controller{
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		String no = request.getParameter("no");//게시판번호
 		QnABoard qnaBoard = QnAService.selectByNo(Integer.parseInt(no));
+		
+		String userId = qnaBoard.getCustomer().getId();//no로 게시판을 작성한 사람의 id가져오기
+		Person person = new PersonDAOImpl().selectById(userId);//id로 user의 status 가져오기
+		int userStatus = person.getStatus();
+		
+		request.setAttribute("userStatus", userStatus);//작성자의 status
 		request.setAttribute("qnaBoard", qnaBoard);
 		
 		ModelAndView mv = new ModelAndView();
